42f41c8c1db7e04a4272f01fcde3d44472b95bc4
Dmitry Karvonen <dmitrykarvonen@gmail.com>
2023-03-23 16:16:21 +0300

    feat: add error message wrapper for displaying errors of inputs

--- src/pages/form/form.tsx
+++ src/pages/form/form.tsx
@@ -1,13 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
+import ErrorMessageWrapper from '../../components/error-message-wrapper/error-message-wrapper';
+
 import './form.styles.css';
 
 interface IFormPageProps {
   changeTitle(): void;
 }
 
-export default class Form extends React.Component<IFormPageProps> {
+interface IFormPageState {
+  textErrMsg: string;
+  dateErrMsg: string;
+  selectErrMsg: string;
+  switchErrMsg: string;
+  fileErrMsg: string;
+  checkboxErrMsg: string;
+}
+
+const INITIAL_STATE = {
+  textErrMsg: '',
+  dateErrMsg: '',
+  selectErrMsg: '',
+  switchErrMsg: '',
+  fileErrMsg: '',
+  checkboxErrMsg: '',
+};
+
+export default class Form extends React.Component<IFormPageProps, IFormPageState> {
   textRef: React.RefObject<HTMLInputElement>;
 
   dateRef: React.RefObject<HTMLInputElement>;
@@ -32,6 +52,7 @@ export default class Form extends React.Component<IFormPageProps> {
     this.switchFemaleRef = React.createRef<HTMLInputElement>();
     this.fileRef = React.createRef<HTMLInputElement>();
     this.checkboxRef = React.createRef<HTMLInputElement>();
+    this.state = INITIAL_STATE;
   }
 
   onSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -46,56 +67,76 @@ export default class Form extends React.Component<IFormPageProps> {
   }
 
   render() {
+    const { textErrMsg, dateErrMsg, selectErrMsg, switchErrMsg, fileErrMsg, checkboxErrMsg } =
+      this.state;
     return (
       <div className="form-container">
         <h2>This is the form page</h2>
         <form className="form" onSubmit={this.onSubmit}>
-          <label htmlFor="text-input">
-            Enter full name:{' '}
-            <input type="text" placeholder="Somebody Someone" id="text-input" ref={this.textRef} />
-          </label>
-          <label htmlFor="date-input">
-            Birthday: <input type="date" id="date-input" name="date-input" ref={this.dateRef} />
-          </label>
-          <label htmlFor="select-input">
-            Choose an option:{' '}
-            <select name="select-input" id="select-input" ref={this.selectRef}>
-              <option value="">--Please choose an option--</option>
-              <option value="option-1">Option 1</option>
-              <option value="option-2">Option 2</option>
-              <option value="option-3">Option 3</option>
-            </select>
-          </label>
-          <p>Pick gender:</p>
-          <div className="switch-field">
-            <input
-              type="radio"
-              id="radio-one"
-              name="switcher"
-              value="male"
-              ref={this.switchMaleRef}
-            />
-            <label htmlFor="radio-one">Male</label>
-            <input
-              type="radio"
-              id="radio-two"
-              name="switcher"
-              value="female"
-              ref={this.switchFemaleRef}
-            />
-            <label htmlFor="radio-two">Female</label>
-          </div>
-          <label htmlFor="file-input">Add picture</label>
-          <input type="file" id="file-input" ref={this.fileRef} />
-          <label htmlFor="checkbox-input">
-            I consent to my personal data:
-            <input
-              type="checkbox"
-              name="checkbox-input"
-              id="checkbox-input"
-              ref={this.checkboxRef}
-            />
-          </label>
+          <ErrorMessageWrapper message={textErrMsg}>
+            <label htmlFor="text-input">
+              Enter full name:{' '}
+              <input
+                type="text"
+                placeholder="Somebody Someone"
+                id="text-input"
+                ref={this.textRef}
+              />
+            </label>
+          </ErrorMessageWrapper>
+          <ErrorMessageWrapper message={dateErrMsg}>
+            <label htmlFor="date-input">
+              Birthday: <input type="date" id="date-input" name="date-input" ref={this.dateRef} />
+            </label>
+          </ErrorMessageWrapper>
+          <ErrorMessageWrapper message={selectErrMsg}>
+            <label htmlFor="select-input">
+              Choose an option:{' '}
+              <select name="select-input" id="select-input" ref={this.selectRef}>
+                <option value="">--Please choose an option--</option>
+                <option value="option-1">Option 1</option>
+                <option value="option-2">Option 2</option>
+                <option value="option-3">Option 3</option>
+              </select>
+            </label>
+          </ErrorMessageWrapper>
+          <ErrorMessageWrapper message={switchErrMsg}>
+            <p>Pick gender:</p>
+            <div className="switch-field">
+              <input
+                type="radio"
+                id="radio-one"
+                name="switcher"
+                value="male"
+                ref={this.switchMaleRef}
+              />
+              <label htmlFor="radio-one">Male</label>
+              <input
+                type="radio"
+                id="radio-two"
+                name="switcher"
+                value="female"
+                ref={this.switchFemaleRef}
+              />
+              <label htmlFor="radio-two">Female</label>
+            </div>
+          </ErrorMessageWrapper>
+          <ErrorMessageWrapper message={fileErrMsg}>
+            <label htmlFor="file-input">Add picture</label>
+            <input type="file" id="file-input" ref={this.fileRef} />
+          </ErrorMessageWrapper>
+          <ErrorMessageWrapper message={checkboxErrMsg}>
+            <label htmlFor="checkbox-input">
+              I consent to my personal data:
+              <input
+                type="checkbox"
+                name="checkbox-input"
+                id="checkbox-input"
+                ref={this.checkboxRef}
+              />
+            </label>
+          </ErrorMessageWrapper>
+
           <input type="submit" value="Submit" />
         </form>
       </div>
