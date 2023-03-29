92eebb7eee42fa3ca5c433cdfa0e01cfd29edff7
Dmitry Karvonen <dmitrykarvonen@gmail.com>
2023-03-29 14:33:57 +0300

    refactor: formPage from class component to functional component

--- src/pages/formPage/formPage.tsx
+++ src/pages/formPage/formPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -6,34 +6,20 @@ import { IFormCard } from '../../components/form-card/form-card';
 import FormCardList from '../../components/form-card-list/form-card-list';
 import FormWithHooks from '../../components/form-with-hooks/form-with-hooks';
 
-interface IFormPageState {
-  cards: IFormCard[];
-}
-
-export default class FormPage extends React.Component<unknown, IFormPageState> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = { cards: [] };
-    this.addCard = this.addCard.bind(this);
-  }
-
-  addCard(card: IFormCard) {
-    const { cards } = this.state;
-    this.setState({ cards: [...cards, { ...card, id: Date.now() }] });
+export default function FormPage() {
+  const [cards, setCards] = useState([] as IFormCard[]);
+  const addCard = (newCard: IFormCard) => {
+    setCards([...cards, newCard]);
     toast('Data has been saved', {
       position: 'top-center',
       autoClose: 3000,
     });
-  }
-
-  render() {
-    const { cards } = this.state;
-    return (
-      <main>
-        <FormWithHooks addCard={this.addCard} />
-        <FormCardList cards={cards} />
-        <ToastContainer />
-      </main>
-    );
-  }
+  };
+  return (
+    <main>
+      <FormWithHooks addCard={addCard} />
+      <FormCardList cards={cards} />
+      <ToastContainer />
+    </main>
+  );
 }
